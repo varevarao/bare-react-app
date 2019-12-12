15db90a45f7706e09241620e7d611837ebb9c23d
Varshneya Rao <varevarao@gmail.com>
2019-12-12 11:04:33 +0530

    Added basic React app build.

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+// Render the app into the div with id `root`
+ReactDOM.render(<div></div>, document.getElementById('root'));
